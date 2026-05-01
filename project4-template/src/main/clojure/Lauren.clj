05b3d3a10c9b01a0c1253869c10e010caeceedee
Lauren Taylor <laurenktaylor04@gmail.com>
2026-05-01 13:20:40 -0400

    Update Lauren.clj

--- project4-template/src/main/clojure/Lauren.clj
+++ project4-template/src/main/clojure/Lauren.clj
@@ -5,34 +5,34 @@
 
 (defn member [atm lizt]
 	(cond
-		((empty? lizt) false)
-		((identical? atm (first lizt)) true)
-		:else (recur atm (rest lizt))
+		(empty? lizt) false
+		(identical? atm (first lizt)) true
+		:else recur atm (rest lizt)
 	) ;; cond close
 ) ;; defn close
 
 
 (defn append [lizt1 lizt2]
 	(cond
-		((empty? lizt1) lizt2)
-		:else (cons (first lizt1) (recur (rest lizt1) lizt2))
+		(empty? lizt1) lizt2
+		:else cons (first lizt1) (recur (rest lizt1) lizt2)
 	) ;; cond close
 ) ;; defn close
 
 
 (defn map [fun lizt]
 	(cond
-		((empty? lizt) '())
-		:else (cons (fun (first lizt)) (recur (fun (rest lizt))))
+		(empty? lizt) '()
+		:else cons (fun (first lizt)) (recur fun (rest lizt))
 	) ;; cond close
 ) ;; defn close
 
 
 (defn same [lizt1 lizt2]
 	(cond
-		((empty? lizt1) (empty? lizt2))
-		((empty? lizt2) false)
-		((identical? (first lizt1) (first lizt2)) recur ((rest lizt1) (rest lizt2)))
+		(empty? lizt1) (empty? lizt2)
+		(empty? lizt2) false
+		(identical? (first lizt1) (first lizt2)) recur (rest lizt1) (rest lizt2)
 		:else false
 	) ;; cond close
 ) ;; defn close
@@ -40,8 +40,8 @@
 
 (defn intersect [lizt1 lizt2]
 	(cond
-		((empty? lizt1) '())
-		((member (first lizt1) lizt2) (cons (first lizt1) (recur (rest lizt1) lizt2)))
-		:else (recur (rest lizt1) lizt2)
+		(empty? lizt1) '()
+		(member (first lizt1) lizt2) (cons (first lizt1) (recur (rest lizt1) lizt2))
+		:else recur (rest lizt1) lizt2
 	) ;; cond close
 ) ;; defn close
