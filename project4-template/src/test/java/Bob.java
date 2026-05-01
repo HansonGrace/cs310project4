1dceaf2cef91dc57a619b459d7acfe99f5686a92
GraceByte <69362728+HansonGrace@users.noreply.github.com>
2026-05-01 12:53:44 -0400

    Add JUNIT tests for Laurens member clojure

--- project4-template/src/test/java/Bob.java
+++ project4-template/src/test/java/Bob.java
@@ -1,7 +1,6 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -28,4 +27,38 @@ class Bob {
 		var list = List.of("A", "B", "C", "D", "E");
 		assertEquals("C", Ethan.third(list));
 	}
+
+	//grace made these
+	//member JUNIT tests
+
+	@Test
+	void testLaurenMemberClojureEmpty() {
+		var member = Clojure.var("Lauren", "member");
+		assertEquals(false, member.invoke("A", List.of()));
+	}
+
+	@Test
+	void testLaurenMemberClojureFoundHead() {
+		var member = Clojure.var("Lauren", "member");
+		assertEquals(true, member.invoke("A", List.of("A", "B", "C")));
+	}
+
+	@Test
+	void testLaurenMemberClojureFoundMiddle() {
+		var member = Clojure.var("Lauren", "member");
+		assertEquals(true, member.invoke("B", List.of("A", "B", "C")));
+	}
+
+	@Test
+	void testLaurenMemberClojureFoundTail() {
+		var member = Clojure.var("Lauren", "member");
+		assertEquals(true, member.invoke("C", List.of("A", "B", "C")));
+	}
+
+	@Test
+	void testLaurenMemberClojureNotFound() {
+		var member = Clojure.var("Lauren", "member");
+		assertEquals(false, member.invoke("Z", List.of("A", "B", "C")));
+	}
+
 }
